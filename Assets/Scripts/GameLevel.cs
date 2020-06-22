5c61b19cf0a782e627cc26051468b91fb272ed81
Weinsone <Weinsone@outlook.com>
2020-06-22 23:26:04 +0500

    Улучшение InputHandler

--- Assets/Scripts/GameLevel.cs
+++ Assets/Scripts/GameLevel.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class GameLevel : MonoBehaviour { // ща по жопе получишь. ой извените
     public static Client LocalPlayer { get; private set; }
+    public static string PidoraTest { get; private set; } = "vy gej";
     public static bool multiplayerMode;
+    InputHandler handler = new InputHandler();
 
     void Start() {
         LocalPlayer = new Client(Server.clients.Count, "Player", Privileges.admin);
+        handler.Initialize(LocalPlayer);
         if (multiplayerMode) {
             // траханье с сокетами (мммм дельфи) (ахахах чую можно будет определить мой код по var'ам)
         }
+    }
+
+    void Update() {
+        handler.ReadInput();
+    }
+
+    void LateUpdate() {
+        handler.ReadViewInput();
     }
 }
--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Только для локального игрока
-public class InputHandler : MonoBehaviour
+public class InputHandler
 {
     Controller playerController; // Local player
 
-    void Start() {
-        playerController = GameLevel.LocalPlayer.GetController().GetComponent<Controller>(); // Получение контроллера локального игрока (конроллер - визуальное продествалени игрока)
+    public void Initialize(Client player) { // гглянь в GameLevel
+        playerController = player.GetController().GetComponent<Controller>(); // Получение контроллера локального игрока (конроллер - визуальное продествалени игрока)
     }
 
-    void Update() {
+    public void ReadInput() { 
         playerController.Move(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); // 1 - нажата W, -1 - нажата S, аналогично с A, D
         Debug.Log(Input.GetAxis("Horizontal")); Debug.Log(Input.GetAxis("Vertical"));
 
@@ -21,7 +21,7 @@ public class InputHandler : MonoBehaviour
         }
     }
 
-    void LateUpdate() {
+    public void ReadViewInput() {
         playerController.View(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
         Debug.Log(Input.GetAxis("Mouse X")); Debug.Log(Input.GetAxis("Mouse Y"));
     }
