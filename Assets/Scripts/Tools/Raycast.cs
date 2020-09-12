8a817f874c8a919fa78573c10a609170fde46928
weinsone <weinsone@outlook.com>
2020-09-12 14:15:58 +0500

    Теперь игра определяет в каким компом игрок управляет

--- Assets/Scripts/Tools/Raycast.cs
+++ Assets/Scripts/Tools/Raycast.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public static class Raycast
 {
-    public static RaycastHit GetRayHit(Ray ray, int layer = 1 << 0) {
-        RaycastHit hit;
+    private static RaycastHit hit;
+    private static PointerEventData pointerData = new PointerEventData(EventSystem.current);
+
+    public static bool GetHitGameObjectByTag(Ray ray, string tag, out GameObject foundObject, int layer = 1 << 0) {
+        hit = GetHit(ray);
+        Debug.Log(hit.collider.tag);
+        if (hit.collider != null && hit.collider.tag == tag) {
+            foundObject = hit.collider.gameObject;
+            return true;
+        } else {
+            foundObject = null;
+            return false;
+        }
+    }
+
+    public static RaycastHit GetHit(Ray ray, int layer = 1 << 0) {
         if (Physics.Raycast(ray, out hit)) {
             return hit;
         }
@@ -13,12 +28,25 @@ public static class Raycast
         return default;
     }
 
-    public static RaycastHit GetRayHit(Vector3 origin, Vector3 direction, int layer = 1 << 0) { // эТА шТУкА ЧЁТО НЕ раБотаЕТ
-        RaycastHit hit;
+    public static RaycastHit GetHit(Vector3 origin, Vector3 direction, int layer = 1 << 0) { // эТА шТУкА ЧЁТО НЕ раБотаЕТ
         if (Physics.Raycast(origin, direction, out hit)) {
             return hit;
         }
         Debug.LogError("Чё-то не так");
         return default;
+    }
+
+    public static GameObject GetUIHit(Vector3 cursorPosition) {
+        pointerData.position = cursorPosition;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerData, results);
+
+        if (results.Count > 0) {
+             return results[0].gameObject;
+        } else {
+            Debug.Log("UI не найден");
+        }
+        return null;
     }
 }
